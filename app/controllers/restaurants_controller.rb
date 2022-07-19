068ec6e7cc73b1df034684421ee005cc4803b495
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-07-19 22:25:23 +0100

    added new and create review method and form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,10 +14,13 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    @restaurant = Restaurant.create(restaurant_params)
-    redirect_to restaurant_path(@restaurant)
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-
   private
 
   def restaurant_params
