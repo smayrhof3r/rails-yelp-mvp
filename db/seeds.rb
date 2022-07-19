fcef685d60f13ce91239e9b0ed82fc7081a14064
Sheila Mayrhofer <smayrhofer@gmail.com>
2022-07-19 20:38:40 +0100

    database seeded with restaurants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+30.times do
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category:,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code
+  )
+
+  5.times do
+    rating = (0..5).to_a.sample
+    Review.create!(
+      content: Faker::Restaurant.review,
+      rating:,
+      restaurant:
+    )
+  end
+
+  puts "Restaurant #{restaurant.id} & 5 Review created"
+end
